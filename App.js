6a6f0d9e8532f2a86de3407ed999950cc060bc9f
Simo Siren <simo.siren@stud.pointcollege.fi>
2020-11-16 00:03:39 +0200

    puolimatkan krouvissa ollaan tän kanssa

--- App.js
+++ App.js
@@ -1,21 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, ListView } from 'react-native';
 
-export default function App() {
+const App = () => {
+
+  const [tavara, setTavara] = useState('')
+  const [lista, setLista] = useState(['maito', 'leipä'])
+
+  const onInputChange = (enteredText) => {
+    setTavara(enteredText)
+  }
+
+  const onAddPress = () => {
+    setLista([...lista, tavara])
+    console.log(lista)
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+
+      <View style={styles.inputAndBtn}>
+
+        <TextInput placeholder="Kirjoita uusi tavara.."
+          style={styles.inputField} onChangeText={onInputChange} />
+
+        <Button color="#f120ff" title="+" onPress={onAddPress} />
+
+      </View>
+      <View>
+        <Text />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 30, marginTop: 25
   },
-});
+  inputAndBtn: {
+    flexDirection: 'row', justifyContent: 'space-between', alignItems: "center"
+  },
+  inputField: {
+    borderBottomColor: "silver", borderBottomWidth: 1, color: "brown", width: '92%', fontSize: 26
+  }
+
+})
+
+export default App
