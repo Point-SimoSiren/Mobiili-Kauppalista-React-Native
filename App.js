fb457d9c7adfd89bcfc882f1f78324b9efd0b5b9
Simo Siren <simo.siren@stud.pointcollege.fi>
2020-11-16 15:52:29 +0200

    flat list pykälässä

--- App.js
+++ App.js
@@ -1,37 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput, ListView } from 'react-native';
+//import { StatusBar } from 'expo-status-bar'
+import React, { useState } from 'react'
+import {
+  StyleSheet, Text, View, Button, TextInput, FlatList
+} from 'react-native'
 
 const App = () => {
 
   const [tavara, setTavara] = useState('')
-  const [lista, setLista] = useState(['maito', 'leipä'])
+  const [lista, setLista] = useState([])
 
   const onInputChange = (enteredText) => {
     setTavara(enteredText)
   }
 
   const onAddPress = () => {
-    setLista([...lista, tavara])
-    console.log(lista)
+    setLista(vanhaLista => [
+      ...vanhaLista,
+      { key: Math.random().toString(), value: tavara }
+    ])
+    setTavara('')
   }
 
   return (
     <View style={styles.screen}>
 
       <View style={styles.inputAndBtn}>
-
         <TextInput placeholder="Kirjoita uusi tavara.."
-          style={styles.inputField} onChangeText={onInputChange} />
+          style={styles.inputField} value={tavara} onChangeText={onInputChange} />
 
-        <Button color="#f120ff" title="+" onPress={onAddPress} />
+        <Button color="#f120ff" title="Lisää" onPress={onAddPress} />
+      </View>
 
-      </View>
-      <View>
-        <Text />
-      </View>
+      <FlatList
+        data={lista}
+        renderItem={itemData => (
+          <View>
+            <Text>{itemData.item.value}</Text>
+          </View>
+        )}>
+
+      </FlatList>
+
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -42,7 +53,11 @@ const styles = StyleSheet.create({
     flexDirection: 'row', justifyContent: 'space-between', alignItems: "center"
   },
   inputField: {
-    borderBottomColor: "silver", borderBottomWidth: 1, color: "brown", width: '92%', fontSize: 26
+    borderBottomColor: "silver", borderBottomWidth: 1, color: "brown", width: '85%', fontSize: 26
+  },
+  listItem: {
+    padding: 10,
+    borderBottomColor: "silver", borderBottomWidth: 1, width: '92%', fontSize: 26
   }
 
 })
